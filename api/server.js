bdba0f7ae2e300fe40ee9f8d3b4cead78dc2eefe
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-07-31 20:06:56 -0700

    added message of the day

--- api/server.js
+++ api/server.js
@@ -11,13 +11,15 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shouts = await db('shouts');
-    res.status(200).json(shouts);
+    const messageOfTheDay = process.env.MOTD || "Allo World";
+
+    res.status(200).json({motd: messageOfTheDay, shouts});
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shouts' });
   }
 });
-
+    
 server.post('/', async (req, res) => {
   try {
     const [id] = await db('shouts').insert(req.body);
